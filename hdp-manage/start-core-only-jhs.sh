af4fbd9e17978be23012764867bba4837044900d
rohit2b <hadoopian@gmail.com>
2014-03-31 00:57:11 -0700

    Fixed start JHS script

--- hdp-manage/start-core-only-jhs.sh
+++ hdp-manage/start-core-only-jhs.sh
@@ -1,1 +1,2 @@
-su - mapred -c "export export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec/; /usr/lib/hadoop-mapreduce/sbin/mr-jobhistory-daemon.sh --config /etc/hadoop/conf stop historyserver "
+echo "Start Job History Server"
+su - mapred -c "export export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec/; /usr/lib/hadoop-mapreduce/sbin/mr-jobhistory-daemon.sh --config /etc/hadoop/conf start historyserver "
