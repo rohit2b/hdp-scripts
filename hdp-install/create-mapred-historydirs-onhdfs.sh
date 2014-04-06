bf789c3829a240c6245d9b3dbea5162b7ca1282f
rohit2b <hadoopian@gmail.com>
2014-04-06 00:41:01 -0700

    Fixed script to run commands as HDFS super user

--- hdp-install/create-mapred-historydirs-onhdfs.sh
+++ hdp-install/create-mapred-historydirs-onhdfs.sh
@@ -3,13 +3,13 @@
 echo "Create mapreduce directories on HDFS in /mapred/history/ location"
 echo "These commands need to be run as HDFS superuser"
 
-hadoop fs -mkdir -p /mr-history/tmp
-hadoop fs -chmod -R 1777 /mr-history/tmp
-hadoop fs -mkdir -p /mr-history/done
-hadoop fs -chmod -R 1777 /mr-history/done
-hadoop fs -chown -R mapred:hdfs /mr-history
+su - hdfs -c "hadoop fs -mkdir -p /mr-history/tmp"
+su - hdfs -c "hadoop fs -chmod -R 1777 /mr-history/tmp"
+su - hdfs -c "hadoop fs -mkdir -p /mr-history/done"
+su - hdfs -c "hadoop fs -chmod -R 1777 /mr-history/done"
+su - hdfs -c "hadoop fs -chown -R mapred:hdfs /mr-history"
 
-hadoop fs -mkdir -p /app-logs
-hadoop fs -chmod -R 1777 /app-logs 
-hadoop fs -chown yarn /app-logs 
+su - hdfs -c "hadoop fs -mkdir -p /app-logs"
+su - hdfs -c "hadoop fs -chmod -R 1777 /app-logs"
+su - hdfs -c "hadoop fs -chown yarn /app-logs"
 
