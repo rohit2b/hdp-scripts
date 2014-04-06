4823ee5d85921a254fcf1799b2923d88b81f8eb6
rohit2b <hadoopian@gmail.com>
2014-04-06 09:57:39 -0700

    Added ATS start to start all core services scrupt

--- hdp-manage/start-all-core-services.sh
+++ hdp-manage/start-all-core-services.sh
@@ -9,3 +9,6 @@ su - yarn -c "export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec; /usr/lib/hadoop
 
 echo "Start Job History Server"
 su - mapred -c "export export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec/; /usr/lib/hadoop-mapreduce/sbin/mr-jobhistory-daemon.sh --config /etc/hadoop/conf start historyserver "
+
+echo "Start YARN Timeline Server"
+su - yarn -c "export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec; /usr/lib/hadoop-yarn/sbin/yarn-daemon.sh --config /etc/hadoop/conf start historyserver"
