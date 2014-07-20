e41f9645ad418340e08a559b9f6685279f12720f
rohit2b <hadoopian@gmail.com>
2014-07-20 11:39:21 -0700

    Remove redundant Hadoop dir creation scripts

--- hdp-install/create-core-yarn-dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./users.sh
-source ./directories.sh
-
-mkdir -p $YARN_LOCAL_DIR;
-chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOCAL_DIR;
-chmod -R 755 $YARN_LOCAL_DIR;
--- hdp-install/create-snn-dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./users.sh
-source ./directories.sh
-
-mkdir -p $FS_CHECKPOINT_DIR;
-chown -R $HDFS_USER:$HADOOP_GROUP $FS_CHECKPOINT_DIR;
-chmod -R 755 $FS_CHECKPOINT_DIR;
