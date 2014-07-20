9d81768fcb1ce28bd41a6c911f8d88875570e1e9
rohit2b <hadoopian@gmail.com>
2014-07-20 11:34:29 -0700

    Cleaned up and organized Hadoop directory create scripts

--- hdp-install/create-core-mapred-log-pid-dir.sh
+++ hdp-install/create-core-mapred-log-pid-dir.sh
@@ -3,20 +3,12 @@
 source ./users.sh
 source ./directories.sh
 
-echo "Create Yarn local log dir"
-mkdir -p $YARN_LOG_DIR;
-chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOG_DIR;
-chmod -R 755 $YARN_LOG_DIR;
-
-mkdir -p $YARN_PID_DIR;
-chown -R $YARN_USER:$HADOOP_GROUP $YARN_PID_DIR;
-chmod -R 755 $YARN_PID_DIR;
-
+echo "Create MapRed log dir"
 mkdir -p $MAPRED_LOG_DIR;
 chown -R $MAPRED_USER:$HADOOP_GROUP $MAPRED_LOG_DIR;
 chmod -R 755 $MAPRED_LOG_DIR;
 
-— MapReduce service PID dir
+echo "Create MapRed pid dir"
 mkdir -p $MAPRED_PID_DIR;
 chown -R $MAPRED_USER:$HADOOP_GROUP $MAPRED_PID_DIR;
 chmod -R 755 $MAPRED_PID_DIR;
