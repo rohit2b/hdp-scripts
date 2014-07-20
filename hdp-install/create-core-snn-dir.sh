21e38338d9197274aed97f36c904d5002e499257
rohit2b <hadoopian@gmail.com>
2014-07-20 12:05:52 -0700

    Fixed SNN create checkpoint directory script

--- hdp-install/create-core-snn-dir.sh
+++ hdp-install/create-core-snn-dir.sh
@@ -1,5 +1,9 @@
-source usersAndGroups.sh
-source directories.sh
+#!/bin/bash
+
+source ./users.sh
+source ./directories.sh
+
+echo "Create Secondary NN checkpoint dir"
 
 mkdir -p $FS_CHECKPOINT_DIR;
 chown -R $HDFS_USER:$HADOOP_GROUP $FS_CHECKPOINT_DIR;
