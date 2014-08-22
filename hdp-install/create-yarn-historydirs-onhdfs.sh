d95286c7a15c4a0f46e52a05f51c6a1b8df25adb
rohit2b <hadoopian@gmail.com>
2014-08-22 06:25:37 -0700

    Updated configs and install script to accomodate for ATS

--- hdp-install/create-yarn-historydirs-onhdfs.sh
+++ hdp-install/create-yarn-historydirs-onhdfs.sh
@@ -5,11 +5,4 @@ echo "These commands need to be run as HDFS superuser"
 
 su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -mkdir -p /yarn/generic-history"
 su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -chmod -R 1777 /yarn/generic-history"
-# su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -mkdir -p /mr-history/done"
-# su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -chmod -R 1777 /mr-history/done"
 su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -chown -R yarn:hdfs /yarn"
-
-# su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -mkdir -p /app-logs"
-# su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -chmod -R 1777 /app-logs"
-# su - hdfs -c "/tmp/hdp/hadoop/bin/hadoop fs -chown yarn /app-logs"
-
